2954ddf6ebba6ed1051a1ce66b101b164dbf7bcf
Sergey Bochkov <bochkovsa@gmail.com>
2024-02-21 18:27:05 +0500

    modern font

--- src/main/java/samygo/App.java
+++ src/main/java/samygo/App.java
@@ -2,7 +2,9 @@ package samygo;
 
 import javax.swing.*;
 
+import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.fonts.inter.FlatInterFont;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -38,6 +40,12 @@ public class App {
 
     public static void main(String[] args) {
         System.setProperty("apple.laf.useScreenMenuBar", "true");
+        System.setProperty("apple.awt.application.name", "SamyGO Channel Editor");
+        System.setProperty( "apple.awt.application.appearance", "system" );
+        FlatInterFont.install();
+        FlatLaf.setPreferredFontFamily(FlatInterFont.FAMILY);
+        FlatLaf.setPreferredLightFontFamily(FlatInterFont.FAMILY_LIGHT);
+        FlatLaf.setPreferredSemiboldFontFamily(FlatInterFont.FAMILY_SEMIBOLD);
         FlatLightLaf.setup();
         new SpringApplicationBuilder(App.class)
                 .headless(false)
